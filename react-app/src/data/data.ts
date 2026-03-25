046531ab8630737491d1de085051fa590bcbcb16
ThefrogerMan <67283743+ThefrogerMan@users.noreply.github.com>
2026-03-25 23:01:10 +0100

    Some more data stuff with updates

--- react-app/src/data/data.ts
+++ react-app/src/data/data.ts
@@ -1,4 +1,9 @@
 import earlyBird from "../assets/early-bird.png";
+import gameOfGnomes from "../assets/game-of-gnomes.png";
+import grimFate from "../assets/grim-fate-awaits.png";
+import lilWiz from "../assets/lilwizz.png";
+import spite from "../assets/spite-oathbound.png";
+import pawnGambit from "../assets/pawns-gambit.png";
 
 export type ProjectType = {
   id: string;
@@ -14,39 +19,47 @@ export type ProjectType = {
 export const projects: ProjectType[] = [
   {
     id: "test",
-    name: "My test project",
+    name: "Early Bird",
     featured: true,
     description: "This game is the greatest, all my friends say so",
-    myContributions: "everything",
+    myContributions: "Animations",
     coverImage: earlyBird,
   },
   {
     id: "test2",
-    name: "My test project 2",
+    name: "Pawns Gambit",
     description: "This game is the greatest, all my friends say so",
-    myContributions: "everything",
-    coverImage: earlyBird,
+    myContributions: "UI",
+    coverImage: pawnGambit,
   },
   {
     id: "test3",
-    name: "My test project 3",
+    name: "Grim Fates Await",
     description: "This game is the greatest, all my friends say so",
-    myContributions: "everything",
-    coverImage: earlyBird,
+    myContributions: "UI",
+    coverImage: grimFate,
   },
   {
     id: "test4",
-    name: "My test project 4",
+    name: "Game of Gnomes",
     description: "This game is the greatest, all my friends say so",
-    myContributions: "everything",
-    coverImage: earlyBird,
+    myContributions: "Movement, combat, animations",
+    coverImage: gameOfGnomes,
   },
   {
     id: "test5",
-    name: "My test project 5",
+    name: "Spite: Oathbound",
     featured: true,
     description: "This game is the greatest, all my friends say so",
-    myContributions: "everything",
-    coverImage: earlyBird,
+    myContributions: "Combat, sound, tools, ",
+    coverImage: spite,
+  },
+  {
+    id: "test6",
+    name: "Merle: The Incompetent Wizard",
+    featured: true,
+    description: "This game is the greatest, all my friends say so",
+    myContributions: "Movement, Gameplay features",
+    coverImage: lilWiz,
   },
 ];
