844aebc4ea5f7b525a3505988b7746f06600163f
ThefrogerMan <67283743+ThefrogerMan@users.noreply.github.com>
2026-03-29 15:08:46 +0200

    new sepc

--- react-app/src/pages/Specialisation.tsx
+++ react-app/src/pages/Specialisation.tsx
@@ -1,14 +1,16 @@
 import earlyIteration from "../assets/Spec/first iteration.gif";
 import screenIteration from "../assets/Spec/talkingGif.gif";
 import workingGIF from "../assets/Spec/Slow type.gif";
+import walkingGIF from "../assets/Spec/walkingDead.gif";
 import dialogV1 from "../assets/Spec/dialogBoxV1.png";
-import dialogV2 from "../assets/Spec/dialogBoxV2.png";
+// import dialogV2 from "../assets/Spec/dialogBoxV2.png";
 import dialogV3 from "../assets/Spec/dialogBoxV3.png";
 import dialogV4 from "../assets/Spec/dialogBoxV4.png";
+import dialogPop from "../assets/Spec/dialogBoxPopUp.png";
 
 export default function Specialisation() {
   return (
-    <div className="grid h-48 grid-cols-2 place-content-center gap-4">
+    <div className="">
       <h1 className="text-center my-12 mt-6 text-4xl">Specialisation: Dialog Tool</h1>
       <Section title="Background" image={workingGIF}>
         <p>
@@ -19,15 +21,16 @@ export default function Specialisation() {
         </p>
       </Section>
       <Section title="Make it work">
-          <p className="min-w-2/4">
+          <img src={earlyIteration} alt={earlyIteration.toString()} className="float-right w-1/2 ml-6 mb-4"  />
+          <img src={dialogV1} alt={dialogV1.toString()} className="float-right w-1/2 ml-6 mb-4" />
+
+          <p className="min-w-1/4">
             To work iterativly and early test is something that TGA is pushing and I have learned from previous experiences. 
             So the first step was just to make a system that can print text in console wit the minimum scope of my project.
             The minimum I had for the project was to have the ability to go through a dialog tree, with different speed on dialog and
             if needed options to differnet branches in the dialog tree. A hard coded exemple of the class structure is used to test 
-            input, ease of building the tree and look of the dialog speed. ** Insert ealry gif of dialog tree, rightside**
-          </p>
-          <img src={dialogV1} alt={dialogV1.toString()} className="min-w-1/4" />
-          <p className="min-w-2/4">
+            input, ease of building the tree and look of the dialog speed. 
+          
             The early testing also made it so extra data that previously wasn't considered needed a place to be stored, like the speaker.
             Once the structure was built and tested with how different dialog would connect it was time to connect it to something visual 
             to easy the use of building the trees. This was made in a visual scripting tool as to save time and not reinventing the wheel
@@ -35,19 +38,21 @@ export default function Specialisation() {
             my groups engine for future projects. The cards that first was added to the visual script was dialog box and criteria.
             The dialog box is what holds what we see on the screen, text, speaker, speed, font, and anything else that has to do
             with the actual written dialog. The criteria is, if wanted, what needs to be fulfilled to have the option of chosing 
-            the next dialog. **picture of early stage dialog tree*
+            the next dialog.
           </p>
-          <img src={earlyIteration} alt={earlyIteration.toString()} className="min-w-1/4" />
-        <p>
+
+        <p className="mt-6">
           Once trees could be made it was time to make possilbe to convert the data from the visual script down to the actual dialog tree.
           This was made through nlohmann JSON reader. This made it so some more data needed not only a increasing ID counter for connecting dialog but 
           using the IDs from the visual scritping to connect them. But after that smaller change it was now possible to connect the tree to actual dialog and 
-          make a text based choose your own adventure. ** Insert image of hotreloading tree**
+          make a text based choose your own adventure.
         </p>
       </Section>
         
       <Section title="Make it Right" image={screenIteration}>
         <p className="min-w-1/2">
+          <img src={dialogV3} alt={dialogV3.toString()} className="float-right w-1/2 ml-6 mb-4" />
+          <img src={dialogPop} alt={dialogPop.toString()} className="float-right w-1/2 ml-6 mb-4" />
           After the bare minimum of the project was done, visually connect dialog and make it easy to update. It is time to make 
           it something that is actually can be used in projects. First thing make it possible to see on screen. 
           Simply getting the text to appear on screen was not to hard but getting it to look good brings out 
@@ -59,22 +64,22 @@ export default function Specialisation() {
           of dialog, for testing and having it as a visable options. The biggest thing that next can be added
           that will give a lot of emotion to the speaker is different fonts. Adding that was a simple drop down from 
           ImGui with a list of given fonts. For even more personality for the speaker sound for either voiced dialog or just
-          a repetable sound woth differnent pitch, similar to banjo kazooie voice linces. ** add final dialog box image**
+          a repetable sound woth differnent pitch, similar to banjo kazooie voice linces.
           
         </p>
-          <img src={dialogV2} alt={dialogV2.toString()} className="min-w-1/4" />
-          <img src={dialogV3} alt={dialogV3.toString()} className="min-w-1/4" />
+
         <p>
           Write about dialog tree connections and conditional dialog trees and ranodm what comes next
         </p>
       </Section>
 
-      <Section title="Make it Pretty" image={workingGIF}>
+      <Section title="Make it Pretty" image={walkingGIF}>
         
-        <img src={dialogV4} alt={dialogV4.toString()} className="min-w-1/4" />
         <p className="min-w-2/4">
+        <img src={dialogV4} alt={dialogV4.toString()} className="float-right w-1/2 ml-6 mb-4" />
           Improvments and wishlist for future building
         </p>
+
       </Section>
     </div>
   );
@@ -92,7 +97,7 @@ function Section({
   return (
     <div className="mb-6 ">
       <h2 className="text-xl font-semibold mb-2">{title}</h2>
-      <img src={image} alt={title} className="w-full h-auto mb-4" />
+      <img src={image} alt={""} className="w-full h-auto mb-4" />
       <div className=" border-gray-300">{children}</div>
     </div>
   );
