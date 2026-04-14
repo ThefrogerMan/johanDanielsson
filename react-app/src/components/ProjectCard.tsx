8b4570f34573c561111e71c09d53c88c4b3daea3
gip <theviktorgip@gmail.com>
2026-04-14 21:40:12 +0200

    nicer cards

--- react-app/src/components/ProjectCard.tsx
+++ react-app/src/components/ProjectCard.tsx
@@ -5,16 +5,31 @@ export default function ProjectCard(props: ProjectType) {
   return (
     <Link
       to={"/projects/" + props.id}
-      className="relative aspect-video text-white group overflow-hidden block font-bold!"
+      className="relative aspect-video text-white group overflow-hidden block font-bold! duration-300 rounded-2xl scale-100 hover:scale-103 hover:shadow transition-transform"
     >
       <div
-        className="absolute inset-0 bg-cover bg-center transition-transform duration-300 scale-100 group-hover:scale-110"
+        className="absolute inset-0 bg-cover bg-center"
         style={{
           backgroundImage: `linear-gradient(to bottom, rgba(0, 0, 0, 1), transparent), url(${props.coverImage})`,
         }}
-      />
-      <div className="relative z-10 p-3 text-white group-hover:underline">
+      >
+        {/* <div
+          className=" w-full h-full bg-cover bg-center rounded-2xl"
+          style={{
+            backgroundImage: ` url(${props.coverImage})`,
+          }}
+        ></div> */}
+      </div>
+      <div className="relative z-10 p-6  text-white flex flex-col  h-full">
         <div>{props.name}</div>
+      </div>
+      <div
+        className="absolute text-white inset-0 bg-cover bg-center opacity-0 group-hover:opacity-100 duration-300 transition-opacity flex flex-col-reverse p-6"
+        style={{
+          backgroundImage: `linear-gradient(to top, rgba(0, 0, 0, 1), transparent)`,
+        }}
+      >
+        <p className="m-0! font-normal ">{props.description}</p>
       </div>
     </Link>
   );
